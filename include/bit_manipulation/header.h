8b2af52597f315197c64e107925f47f629915ea5
dtpandya <daiviktpandya@outlook.com>
2026-03-14 21:24:01 +0530

    first question of bit manipulation

---
+++ include/bit_manipulation/header.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void setBit(uint32_t *number, uint8_t *bitPosition);
+
+#endif
--- main.c
+++ main.c
@@ -1,3 +1,15 @@
-int main(){
-    
+/**************************************************************
+* Discription : main file for bitmanipulation
+**************************************************************/
+#include "include/bit_manipulation/header.h"
+int main(void){
+	uint32_t number = 0;
+	uint8_t bitPosition = 0;
+	printf("Enter number : ");
+	scanf("%x", &number);
+	printf("Enter bit position :");
+        scanf("%hhd", &bitPosition);
+	setBit(&number, &bitPosition);
+	printf("%x\n",number);
+	return 0;
 }

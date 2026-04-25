179b24fa816c6e161edc43a5716b01d5af881d7d
dtpandya <daiviktpandya@outlook.com>
2026-04-25 12:26:55 +0530

    corrected UDT/ex01_sensor_record/sensor_record.c

--- UDT/ex01_sensor_record/sensor_record.c
+++ UDT/ex01_sensor_record/sensor_record.c
@@ -21,23 +21,23 @@ void takeInput(Sensor *sensor){
 	printf("For status --> OK=0, WARN=1, FAIL=2\n");
 	for(int i = 1; i <= TOATAL_SENSORS; i++){
 		printf("Enter ID of sensor %d : ",i);
-		scanf("%hd", &sensor[i-1].id);
+		scanf("%hd", &(sensor+i-1)->id);
 		printf("Enter name of sensor %d : ",i);
-		scanf("%s", sensor[i-1].name);
+		scanf("%s", (sensor+i-1)->name);
 		printf("Enter reading values of sensor %d : ",i);
-		scanf("%f", &sensor[i-1].readingValue);
+		scanf("%f", &(sensor+i-1)->readingValue);
 		printf("Enter status of sensor %d : ", i);
-		scanf("%u", &sensor[i-1].status);
+		scanf("%u", &(sensor+i-1)->status);
 	}
 }
 
 void printData(Sensor *sensor){
 	for(int i=1; i <= TOATAL_SENSORS; i++){
-		if(sensor[i-1].status != FAIL){
-			printf("Readings of %s sensor with ID : %hd is %f.\n",sensor[i-1].name, sensor[i-1].id, sensor[i-1].readingValue);
+		if((sensor+i-1)->status != FAIL){
+			printf("Readings of %s sensor with ID : %hd is %f.\n",(sensor+i-1)->name, (sensor+i-1)->id, (sensor+i-1)->readingValue);
 		}
 		else{
-			printf("%s sensor with ID : %hd is failed.\n",sensor[i-1].name, sensor[i-1].id);
+			printf("%s sensor with ID : %hd is failed.\n",(sensor+i-1)->name, (sensor+i-1)->id);
 		}
 	}
 }
